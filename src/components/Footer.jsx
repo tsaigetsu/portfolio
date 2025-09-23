ee0391fb9bfb57d51ab3c2d573f3937f36a96430
Vladyslav Aka Tsaigetsu <152321404+tsaigetsu@users.noreply.github.com>
2025-09-23 13:12:19 +0200

    tried to make icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,40 @@
+import React from 'react';
+import Icons from '../assets/symbol-defs.svg?raw';
+
 export default function(){
     return(
-        <section className="footer-section">
+        <footer className="footer-section">
             <div className="footer-wrapper">
                 <h2 className="second-heading">Ready to increase your sales?<br/> Contact me</h2>
                 <div className="footer-links-wrapper">
                     <ul className="footer-links">
-                        <li className="footer-links-items"><a href="mailto:vstremetskyi@gmail.com?subject=Partnership%20Proposal" className="footer-links-link">vstremetskyi@gmail.com</a></li>
-                        <li className="footer-links-items"><a href="https://t.me/lcc_wladislav" className="footer-links-link">@lcc_wladislav</a></li>
-                        <li className="footer-links-items"><a href="https://www.instagram.com/lcc_wladislav" className="footer-links-link">@lcc_wladislav</a></li>
+                        <li className="footer-links-items">
+                            <a href="mailto:vstremetskyi@gmail.com?subject=Partnership%20Proposal" className="footer-links-link">
+                            {/* <svg className="footer-links-icon">
+                                <use href={`${Icons}#icon-email`} />
+                            </svg> */}
+                            vstremetskyi@gmail.com
+                            </a>
+                        </li>
+                        <li className="footer-links-items">
+                            <a href="https://t.me/lcc_wladislav" className="footer-links-link">
+                            {/* <svg className="footer-links-icon">
+                                <use href={`${Icons}#icon-telegram`} />
+                            </svg> */}
+                            @lcc_wladislav
+                            </a>
+                        </li>
+                        <li className="footer-links-items">
+                            <a href="https://www.instagram.com/lcc_wladislav" className="footer-links-link">
+                            {/* <svg className="footer-links-icon">
+                                <use href={`${Icons}#icon-instagram`} />
+                            </svg> */}
+                            @lcc_wladislav
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </section>
+        </footer>
     )
 }
